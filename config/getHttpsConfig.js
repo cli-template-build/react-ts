c74758684a6164225497428472ef2efc294ede03
yanlele <331393627@qq.com>
2020-06-20 19:05:07 +0800

    feat: validateKeyAndCerts

--- config/getHttpsConfig.js
+++ config/getHttpsConfig.js
@@ -4,4 +4,23 @@ const crypto = require('crypto');
 const chalk = require('react-dev-utils/chalk');
 const paths = require('./paths');
 
-
+const validateKeyAndCerts = params => {
+  const { cert, key, keyFile, crtFile } = params;
+  let encrypted;
+  try {
+    encrypted = crypto.publicEncrypt(cert, Buffer.from('test'));
+  } catch (err) {
+    throw new Error(
+      `The certificate "${chalk.yellow(crtFile)}" is invalid.\n${err.message}`,
+    );
+  }
+  try {
+    crypto.privateDecrypt(key, encrypted);
+  } catch (err) {
+    throw new Error(
+      `The certificate key "${chalk.yellow(keyFile)}" is invalid.\n${
+        err.message
+      }`,
+    );
+  }
+};
