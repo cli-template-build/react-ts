4975e51833053df759e7ceb5f8502896e9165738
yanlele <331393627@qq.com>
2020-06-20 19:46:22 +0800

    feat: 添加gethpptsconfig.js

--- config/getHttpsConfig.js
+++ config/getHttpsConfig.js
@@ -8,6 +8,11 @@ const validateKeyAndCerts = params => {
   const { cert, key, keyFile, crtFile } = params;
   let encrypted;
   try {
+    /*
+      crypto.publicEncrypt(key, buffer)
+      加密， 返回一个加密的结果
+    * */
+    // 加密
     encrypted = crypto.publicEncrypt(cert, Buffer.from('test'));
   } catch (err) {
     throw new Error(
@@ -15,6 +20,7 @@ const validateKeyAndCerts = params => {
     );
   }
   try {
+    // 解密
     crypto.privateDecrypt(key, encrypted);
   } catch (err) {
     throw new Error(
@@ -23,4 +29,32 @@ const validateKeyAndCerts = params => {
       }`,
     );
   }
+};
+
+const readEnvFile = (file, type) => {
+  if (!fs.existsSync(file)) {
+    throw new Error(
+      `You specified ${chalk.cyan(
+        type,
+      )} in your env, but the file "${chalk.yellow(file)}" can't be found.`,
+    );
+  }
+  return fs.realpathSync(file);
+};
+
+const getHttpsConfig = () => {
+  const { SSL_CRT_FILE, SSL_KEY_FILE, HTTPS } = process.env;
+  const isHttps = HTTPS === 'true';
+  if (isHttps && SSL_CRT_FILE && SSL_KEY_FILE) {
+    const crtFile = path.resolve(paths.appPath, SSL_CRT_FILE);
+    const keyFile = path.resolve(paths.appPath, SSL_KEY_FILE);
+    const config = {
+      cert: readEnvFile(crtFile, 'SSL_CRT_FILE'),
+      key: readEnvFile(keyFile, 'SSL_KEY_FILE'),
+    };
+    // 其实主要目的就是验证加解密是否成功
+    validateKeyAndCerts({ ...config, keyFile, crtFile });
+    return config;
+  }
+  return isHttps;
 };
